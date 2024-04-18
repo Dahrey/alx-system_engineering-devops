67447fa4ab110d767cfab6cb3c9d524d18bdbbf5
Dahrey <138259597+Dahrey@users.noreply.github.com>
2024-04-18 05:53:07 +0100

    Update setup.sql

--- 0x14-mysql/setup.sql
+++ 0x14-mysql/setup.sql
@@ -1,4 +1,4 @@
-REATE USER holberton_user@localhost IDENTIFIED BY "projectcorrection280hbtn";
+CREATE USER holberton_user@localhost IDENTIFIED BY "projectcorrection280hbtn";
 GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 CREATE DATABASE tyrell_corp;
 USE tyrell_corp;
